4f80c2997423751f2013e47610400a1e3691cc73
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-12-16 15:26:30 +0000

    Add post functionality to add contact

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let persons = [
   {
@@ -52,6 +53,32 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter((person) => person.id !== id);
   response.status(204).end();
+});
+
+const generateNumber = () => {
+  return Math.floor(Math.random() * 100000000000) + 4;
+};
+
+app.post("/api/persons", (request, response) => {
+  const id = generateNumber();
+
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "Content missing",
+    });
+  }
+
+  const person = {
+    id: id,
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
